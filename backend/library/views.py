ecbc93cebd35a7a8b9d95173a57dc0682496ce7f
mrToad <korostylev.ov@gmail.com>
2022-02-19 21:36:12 +0300

    init commit

---
+++ backend/library/views.py
@@ -0,0 +1,8 @@
+from rest_framework.viewsets import ModelViewSet
+from .serializers import AuthorModelSerializer
+from .models import Author
+
+
+class AuthorViewSet(ModelViewSet):
+    serializer_class = AuthorModelSerializer
+    queryset = Author.objects.all()
